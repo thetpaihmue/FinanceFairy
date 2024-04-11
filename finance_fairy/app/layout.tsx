58324cffdd3167ad32ae5949d2f0143698cb18a9
Thet Paing Hmue <thetpainghmue@cmhl.com.mm>
2024-04-11 09:14:36 +0630

    add title and description

---
+++ finance_fairy/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Finance-Fairy",
+  description:
+    "Let Finance-Fairy wave its wand and make finances fun for everyone!.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
